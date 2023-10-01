aecc380483a372da51684edf96763b42a73d4fa5
iceeburr <iceeburr@gmail.com>
2023-10-01 22:32:42 +0300

    final reinit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "</> Coding with Love 🩵",
+  description: "Everything starts off somewhere. Let's start our journey to make the world a better place for our future.",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
