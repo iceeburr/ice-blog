8754bf2f269fe5f97f699ed3ac07e31417dd0bd6
iceeburr <iceeburr@gmail.com>
2023-10-04 22:04:13 +0300

    added div :skull:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hey, I&apos;m iceeburr.</h1>
-      <p>Let me introduce myself.</p>
-      <p>Still in progress. <br/><br/>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit fuga laudantium repellendus nisi, dignissimos quisquam! Quae maiores alias, odio obcaecati ratione error molestias deleniti accusantium eveniet voluptate iure excepturi dicta.</p>
+      <div>
+        <h1>Hey, I&apos;m iceeburr.</h1>
+        <p>Let me introduce myself.</p>
+        <p>Still in progress. <br/><br/>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit fuga laudantium repellendus nisi, dignissimos quisquam! Quae maiores alias, odio obcaecati ratione error molestias deleniti accusantium eveniet voluptate iure excepturi dicta.</p>
+      </div>
     </main>
   )
 }
